f391995efdf990945e2f8c1a96fc7fcb56b84afa
Lotte De vries <ldevries38@st.noorderpoort.nl>
2022-06-07 21:05:24 +0200

    contact + info responsive

--- src/Components/notFound/NotFound.js
+++ src/Components/notFound/NotFound.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 const page404 = () => {
     return <section className="bg-404Icon h-screen w-full bg-no-repeat bg-cover flex justify-center items-center">
-        <div className="container mx-auto flex items-end justify-center h-3/5">
+        <div className="container mx-auto flex items-end justify-center h-3/5 ">
         <div className="flex flex-col font-barlow w-2/4 justify-center items-center ">
             <h1 className="text-white text-heading1 font-bold">Oops...</h1>
-            <h2 className="text-white font-bold text-heading3 mt-1">404 - Pagina niet gevonden</h2>
-            <p className="text-white text-paragraphL w-10/12 text-center mt-1">Momenteel is de pagina niet beschikbaar.
+            <h2 className="text-white font-bold text-heading3 mt-1 text-center">404 - Pagina niet gevonden</h2>
+            <p className="text-white text-paragraphL w-10/12 text-center mt-1 smMax:w-full">Momenteel is de pagina niet beschikbaar.
                 Excuses voor het ongemak.</p>
 
                 <div className="pt-8">
